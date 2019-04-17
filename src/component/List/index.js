8f91394613f248ad13de8de3b95df67a01b816fc
ariebrainware <publicu001@gmail.com>
2019-04-17 13:47:37 +0700

    Small refactor

--- src/component/List/index.js
+++ src/component/List/index.js
@@ -5,43 +5,44 @@ import axios from 'axios';
 import Icon from '../Icon';
 import Message from '../Message';
 
-class PostList extends React.Component{
+class TopicList extends React.Component{
   state = {
-    post: []
+    topic: [],
   }
 
   fetchData = () => {
-    axios.get('http://localhost:3001/topic').then(response => {
+    const URL = process.env.NODE_ENV !== 'development' ? process.env.REACT_APP_APIPRODUCTION : process.env.REACT_APP_APILOCAL 
+    axios.get(URL).then(response => {
       this.setState({
-        post: response.data
+        topic: response.data,
       })
     })
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.fetchData()
-    }, 1000)
-    console.log(this.state.post)
+    this.fetchData()
   }
+
+  renderEmptyMessage = () => (
+    <Message text='Empty topic list' />
+  )
 
   render() {
     return (
       <List divided relaxed>
         {
-          this.state.post.length !== 0 ? 
-            this.state.post.map(data => (
-              <List.Item>
-                <List.Content>
-                  <List.Header as='a'>{data.text}</List.Header>
-                  <div className="votes"><Icon name='angle up'/>{data.upvotes}</div>
-                </List.Content>
-              </List.Item>
-            )) : <Message text='Empty list post'> {console.log('empty')} </Message>
+          this.state.topic.length !== 0 ? this.state.topic.map(data => (
+            <List.Item>
+              <List.Content>
+                <List.Header as='a'>{data.text}</List.Header>
+                <div className="votes"><Icon name='angle up'/>{data.upvotes}</div>
+              </List.Content>
+            </List.Item>
+          )) : this.renderEmptyMessage
         }
       </List>
     )
   }
 }
 
-export default PostList
+export default TopicList
